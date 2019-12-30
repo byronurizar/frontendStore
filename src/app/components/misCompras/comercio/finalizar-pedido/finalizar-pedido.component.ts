6a736cb9b871d2a0b7fe351786dc119b5d7e07af
Byron Lopez Urizar <blopez@maycom-ad.maycom.com.gt>
2019-12-30 14:24:46 -0600

    Cambios 30122019

--- src/app/components/misCompras/comercio/finalizar-pedido/finalizar-pedido.component.ts
+++ src/app/components/misCompras/comercio/finalizar-pedido/finalizar-pedido.component.ts
@@ -5,6 +5,11 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ProductsService } from 'src/app/shared/service/e-commerce/products.service';
 import { CartService } from 'src/app/shared/service/e-commerce/cart.service';
 import { InvoiceService } from 'src/app/shared/service/e-commerce/invoice.service';
+import { ElementoLista } from 'src/app/modelos/elementoLista.model';
+import { ToastrService } from 'ngx-toastr';
+import { ConectorApi } from 'src/app/servicios/conectorApi.service';
+import { ApiRest } from 'src/app/modelos/apiResponse.model';
+import { Carrito } from 'src/app/servicios/carrito.service';
 
 @Component({
   selector: 'app-finalizar-pedido',
@@ -12,46 +17,145 @@ import { InvoiceService } from 'src/app/shared/service/e-commerce/invoice.servic
   styleUrls: ['./finalizar-pedido.component.scss']
 })
 export class FinalizarPedidoComponent implements OnInit {
-
-  public cartItems: Observable<CartItem[]> = of([]);
-  public checkOutItems: CartItem[] = [];
+  tipoPagoSeleccionado=0;
+  nuevoTipoPago=0;
+  departamentos: ElementoLista[] = [];
+  municipios: ElementoLista[] = [];
+  tiposPago:any[]=[];
+  detallePedido:any[]=[];
+  public cartItems: Observable<any[]> = of([]);
+  public checkOutItems: any;
   public checkoutForm: FormGroup;
   public amount: number;
   public submitted = false;
   public userInfo: string;
-  constructor(private fb: FormBuilder, public productService: ProductsService, private cartService: CartService, private invoiceService: InvoiceService) {
+  constructor(private fb: FormBuilder,private conectorApi: ConectorApi, private toastrService: ToastrService,private carrito:Carrito, public productService: ProductsService, private cartService: CartService, private invoiceService: InvoiceService) {
     this.createForm();
   }
 
   createForm() {
     this.checkoutForm = this.fb.group({
-      firstname: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
-      lastname: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
-      phone: ['', [Validators.required, Validators.pattern('[0-9]+')]],
-      email: ['', [Validators.required, Validators.email]],
-      country: ['', Validators.required],
-      address: ['', [Validators.required, Validators.maxLength(50)]],
-      town: ['', Validators.required],
-      state: ['', Validators.required],
-      postalcode: ['', Validators.required]
+      nombres: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
+      apellidos: ['', [Validators.required, Validators.pattern('[a-zA-Z][a-zA-Z ]+[a-zA-Z]$')]],
+      telefonos: ['', [Validators.required, Validators.pattern('[0-9]+')]],
+      departamento: ['', Validators.required],
+      municipio: ['', Validators.required],
+      direccion: ['', [Validators.required, Validators.minLength(20)]],
+      puntoReferencia: ['', [Validators.required, Validators.minLength(20)]],
     });
+  }
+
+  public cambiarTipoPago(idtipo){
+    this.nuevoTipoPago=idtipo;
   }
   
   onSubmit() {
+    let dataPedido;
     this.submitted = true;
-    if (this.checkoutForm.invalid) {
-      return;
-    }
+    // if (this.checkoutForm.invalid) {
+    //   return;
+    // }
     this.userInfo = this.checkoutForm.value;
-    this.invoiceService.createOrder(this.checkOutItems, this.userInfo, this.amount);
+    console.log("Info formulario",this.userInfo);
+    this.checkOutItems.forEach(item=>{
+     let itemPedido={
+        id:item.producto.id,
+        idTalla:item.talla.idTalla,
+        idColor:item.color.idColor,
+        cantidad:item.cantidad
+      }
+      this.detallePedido.push(itemPedido);
+    })
+
+let json={
+  data:this.userInfo,
+  detallePedido:this.detallePedido,
+  idTipoPago:this.tipoPagoSeleccionado
+}
+
+console.log("Data pedido",json);
+    this.conectorApi.Post("pedido/recibe/registro", json).subscribe(
+      (data)=>{
+        console.log("Data Registro",data);
+      },
+      (dataErrror)=>{
+        console.log("Data Registro Error",dataErrror);
+      }
+    )
   }
   public getTotal(): Observable<number> {
-    return this.cartService.getTotalAmount();
+    return this.carrito.getCantidadTotal();
   }
   ngOnInit() {
-    this.cartItems = this.cartService.getAll();
+    this.cartItems = this.carrito.getTodos();
     this.cartItems.subscribe(products => this.checkOutItems = products);
-    this.getTotal().subscribe(amount => this.amount = amount);
+    this.carrito.getCantidadTotal().subscribe(amount => this.amount = amount);
+    this.listarDepartamentos();
+    this.listarTipoPago();
   }
+  async listarDepartamentos() {
+    try {
+      this.departamentos.push(new ElementoLista('', 'Seleccione un departamento'))
+      this.conectorApi.Get('departamentos/listar').subscribe(
+        async (data) => {
+          let dat = data as ApiRest;
+          console.log("Todos los departamentos", dat.data);
+          await dat.data.forEach(departamento => {
+            this.departamentos.push(new ElementoLista(departamento.id, departamento.descripcion))
+          });
+        },
+        (dataError) => {
+          this.toastrService.error(dataError.error, 'Alerta!');
+        }
+      )
+    } catch (ex) {
+      this.toastrService.error(ex, 'Alerta!');
+    }
+  }
+  
+  async listarMunicipios(event) {
+    try {
+      this.municipios = [];
+      let idDepto = event.target.value;
+      if (idDepto > 0) {
+        this.municipios.push(new ElementoLista('', 'Seleccione un municipio'))
+        this.conectorApi.Get('municipios/listar/departamento/' + idDepto).subscribe(
+          async (data) => {
+            let dat = data as ApiRest;
+            console.log("Todos los municipios", dat.data);
+            await dat.data.forEach(muni => {
+              this.municipios.push(new ElementoLista(muni.id, muni.descripcion))
+            });
+          },
+          (dataError) => {
+            this.toastrService.error(dataError.error, 'Alerta!');
+          }
+        )
+      }
+    } catch (ex) {
+      this.toastrService.error(ex, 'Alerta!');
+    }
+
+  }
+  async listarTipoPago() {
+    try {
+      this.conectorApi.Get('tipopago/listar').subscribe(
+        (data) => {
+          let dat = data as ApiRest;
+          if(dat.codigo==0){
+            this.tiposPago=dat.data;
+          }
+        },
+        (dataError) => {
+          this.toastrService.error(dataError.error, 'Alerta!');
+        }
+      )
+    } catch (ex) {
+      this.toastrService.error(ex, 'Alerta!');
+    }
+
+  }
+
+
 
 }
--- src/app/servicios/conectorApi.service.ts
+++ src/app/servicios/conectorApi.service.ts
@@ -5,7 +5,7 @@ import { ApiRest } from '../modelos/apiResponse.model';
 import { Observable } from 'rxjs/Observable';
 
 const urlBase = environment.urlBase;
-let token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTU3NzE0OTA2NX0.f28vTkwAuTRwMQLLw22PLss5QddKhPcqhRTNmMutSEM';
+let token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjEsImlhdCI6MTU3NzcyNjUwMn0.sVb6otw3Y55OGhNhKNPqsjB57iM9ESkbySy6HGPzexk';
 
 
 // if(sessionStorage.getItem("token")){
